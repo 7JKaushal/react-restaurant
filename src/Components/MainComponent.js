66988af5051bcbaf1e7cb72d46c4c5a7ae86dcc2
Kaushal Joshi <7joshikaushal@gmail.com>
2021-06-15 21:32:17 +0530

    Control Form

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -2,15 +2,17 @@ import { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Contact from "./ContactComponent";
-import { COMMENTS } from "../shared/comments";
-import { PROMOTIONS } from "../shared/promotions";
-import { LEADERS } from "../shared/leaders";
+import About from "./AboutComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -23,6 +25,7 @@ class Main extends Component {
       leaders: LEADERS,
     };
   }
+
   render() {
     const HomePage = () => {
       return (
@@ -33,9 +36,26 @@ class Main extends Component {
         />
       );
     };
+
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
+
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route
@@ -43,7 +63,12 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
-          <Route path="/contactus" component={Contact} />
+          <Route path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Route
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Redirect to="/home" />
         </Switch>
 
